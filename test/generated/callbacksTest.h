07fb1ff5f4c884f89897289094149b5df143a398
Dirk Adler <dirk.adler@idealo.de>
2019-10-04 15:32:03 +0200

    add support for callback type

---
+++ test/generated/callbacksTest.h
@@ -0,0 +1,2 @@
+void set_callback(void(*test_callback)(int param1, int param2));
+
---
+++ test/generated/callbacksTest.php
@@ -0,0 +1,159 @@
+<?php declare(strict_types=1);
+namespace FFIMe\Test;
+use FFIMe\FFIMe;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Note: this is a generated file, do not edit this!!!
+ */
+class callbacksTest extends TestCase {
+
+    const EXPECTED = '<?php namespace %s;
+use FFI;
+interface itest {}
+class test {
+    const SOFILE = \'%s\';
+    const HEADER_DEF = \'void set_callback(void(*test_callback)(int param1, int param2));
+\';
+    private FFI $ffi;
+    public function __construct(string $pathToSoFile = self::SOFILE) {
+        $this->ffi = FFI::cdef(self::HEADER_DEF, $pathToSoFile);
+    }
+    
+    public function cast(itest $from, string $to): itest {
+        if (!is_a($to, itest::class)) {
+            throw new \\LogicException("Cannot cast to a non-wrapper type");
+        }
+        return new $to($this->ffi->cast($to::getType(), $from->getData()));
+    }
+
+    public function makeArray(string $class, array $elements) {
+        $type = $class::getType();
+        if (substr($type, -1) !== "*") {
+            throw new \\LogicException("Attempting to make a non-pointer element into an array");
+        }
+        $cdata = $this->ffi->new(substr($type, 0, -1) . "[" . count($elements) . "]");
+        foreach ($elements as $key => $raw) {
+            $cdata[$key] = $raw === null ? null : $raw->getData();
+        }
+        return new $class($cdata);
+    }
+
+    public function sizeof($classOrObject): int {
+        if (is_object($classOrObject) && $classOrObject instanceof itest) {
+            return $this->ffi->sizeof($classOrObject->getData());
+        } elseif (is_a($classOrObject, itest::class)) {
+            return $this->ffi->sizeof($this->ffi->type($classOrObject::getType()));
+        } else {
+            throw new \\LogicException("Unknown class/object passed to sizeof()");
+        }
+    }
+
+    public function getFFI(): FFI {
+        return $this->ffi;
+    }
+
+    
+    public function __get(string $name) {
+        switch($name) {
+            default: return $this->ffi->$name;
+        }
+    }
+    public function set_callback(?callback $p0): void {
+        $this->ffi->set_callback($p0);
+    }
+}
+
+class string_ implements itest {
+    private FFI\\CData $data;
+    public function __construct(FFI\\CData $data) { $this->data = $data; }
+    public function getData(): FFI\\CData { return $this->data; }
+    public function equals(string_ $other): bool { return $this->data == $other->data; }
+    public function addr(): string_ptr { return new string_ptr(FFI::addr($this->data)); }
+    public function toString(?int $length = null): string { return $length === null ? FFI::string($this->data) : FFI::string($this->data, $length); }
+    public static function getType(): string { return \'char*\'; }
+}
+class string_ptr implements itest {
+    private FFI\\CData $data;
+    public function __construct(FFI\\CData $data) { $this->data = $data; }
+    public function getData(): FFI\\CData { return $this->data; }
+    public function equals(string_ptr $other): bool { return $this->data == $other->data; }
+    public function addr(): string_ptr_ptr { return new string_ptr_ptr(FFI::addr($this->data)); }
+    public function deref(int $n = 0): string_ { return new string_($this->data[$n]); }
+    public static function getType(): string { return \'char**\'; }
+}
+class string_ptr_ptr implements itest {
+    private FFI\\CData $data;
+    public function __construct(FFI\\CData $data) { $this->data = $data; }
+    public function getData(): FFI\\CData { return $this->data; }
+    public function equals(string_ptr_ptr $other): bool { return $this->data == $other->data; }
+    public function addr(): string_ptr_ptr_ptr { return new string_ptr_ptr_ptr(FFI::addr($this->data)); }
+    public function deref(int $n = 0): string_ptr { return new string_ptr($this->data[$n]); }
+    public static function getType(): string { return \'char***\'; }
+}
+class string_ptr_ptr_ptr implements itest {
+    private FFI\\CData $data;
+    public function __construct(FFI\\CData $data) { $this->data = $data; }
+    public function getData(): FFI\\CData { return $this->data; }
+    public function equals(string_ptr_ptr_ptr $other): bool { return $this->data == $other->data; }
+    public function addr(): string_ptr_ptr_ptr_ptr { return new string_ptr_ptr_ptr_ptr(FFI::addr($this->data)); }
+    public function deref(int $n = 0): string_ptr_ptr { return new string_ptr_ptr($this->data[$n]); }
+    public static function getType(): string { return \'char****\'; }
+}
+class void_ptr implements itest {
+    private FFI\\CData $data;
+    public function __construct(FFI\\CData $data) { $this->data = $data; }
+    public function getData(): FFI\\CData { return $this->data; }
+    public function equals(void_ptr $other): bool { return $this->data == $other->data; }
+    public function addr(): void_ptr_ptr { return new void_ptr_ptr(FFI::addr($this->data)); }
+    public static function getType(): string { return \'void*\'; }
+}
+class void_ptr_ptr implements itest {
+    private FFI\\CData $data;
+    public function __construct(FFI\\CData $data) { $this->data = $data; }
+    public function getData(): FFI\\CData { return $this->data; }
+    public function equals(void_ptr_ptr $other): bool { return $this->data == $other->data; }
+    public function addr(): void_ptr_ptr_ptr { return new void_ptr_ptr_ptr(FFI::addr($this->data)); }
+    public function deref(int $n = 0): void_ptr { return new void_ptr($this->data[$n]); }
+    public static function getType(): string { return \'void**\'; }
+}
+class void_ptr_ptr_ptr implements itest {
+    private FFI\\CData $data;
+    public function __construct(FFI\\CData $data) { $this->data = $data; }
+    public function getData(): FFI\\CData { return $this->data; }
+    public function equals(void_ptr_ptr_ptr $other): bool { return $this->data == $other->data; }
+    public function addr(): void_ptr_ptr_ptr_ptr { return new void_ptr_ptr_ptr_ptr(FFI::addr($this->data)); }
+    public function deref(int $n = 0): void_ptr_ptr { return new void_ptr_ptr($this->data[$n]); }
+    public static function getType(): string { return \'void***\'; }
+}';
+
+    protected FFIMe $lib;
+    protected Printer $printer;
+
+    public function setUp(): void {
+        $this->lib = new class(
+            "/lib/x86_64-linux-gnu/libc.so.6",
+            [
+                __DIR__,
+                __DIR__ . '/../include'
+            ]
+        ) extends FFIMe {
+            // Bypass filtering for tests
+            protected function filterSymbolDeclarations(): void {}
+        };
+    }
+
+    public function tearDown(): void {
+        @unlink(__DIR__ . '/callbacksTest.result.php');
+    }
+
+    /**
+     * @textdox Test callbacks
+     */
+    public function testCodeGen() {
+        $this->lib->include(__DIR__ . '/callbacksTest.h');
+        $this->lib->codeGen("test\\test", __DIR__ . '/callbacksTest.result.php');
+        $this->assertFileExists(__DIR__ . '/callbacksTest.result.php');
+        $this->assertStringMatchesFormat(self::EXPECTED, trim(file_get_contents(__DIR__ . '/callbacksTest.result.php')));
+    }
+}
